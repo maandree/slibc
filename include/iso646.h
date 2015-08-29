4f2ea176571f803e1d7de2c7aca7681ec4b17223
Mattias Andrée <maandree@operamail.com>
2015-08-29 05:23:33 +0200

    add iso646.h
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

---
+++ include/iso646.h
@@ -0,0 +1,41 @@
+/**
+ * slibc — Yet another C library
+ * Copyright © 2015  Mattias Andrée (maandree@member.fsf.org)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef _ISO646_H
+#define _ISO646_H
+#include <slibc/version.h>
+
+# warning "Please consider not using <iso646.h>, is really not good for readability."
+
+
+
+#define and     &&
+#define and_eq  &=
+#define bitand  &
+#define bitor   |
+#define compl   ~
+#define not     !
+#define not_eq  !=
+#define or      ||
+#define or_eq   |=
+#define xor     ^
+#define xor_eq  ^=
+
+
+
+#endif
+
