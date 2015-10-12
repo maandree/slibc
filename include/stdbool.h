b5442819f5ee5ad90c64e511363456cc6b0ead38
Mattias Andrée <maandree@operamail.com>
2015-10-13 01:13:04 +0200

    guard stdbool.h with __bool_true_false_are_defined rather than _STDBOOL_H to let the macros to be undefined, and then redefined by including stdbool.h again
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- include/stdbool.h
+++ include/stdbool.h
@@ -15,8 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#ifndef _STDBOOL_H
-#define _STDBOOL_H
+#ifndef __bool_true_false_are_defined
 #include <slibc/version.h>
 #include <slibc/features.h>
 
