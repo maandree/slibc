3da504acf21b33753e0c4260f86c311458aba805
Mattias Andrée <maandree@operamail.com>
2015-11-16 23:03:05 +0100

    typo
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- src/slibc-human/humansize.c
+++ src/slibc-human/humansize.c
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include <slib-human.h>
+#include <slibc-human.h>
 #include <stdlib.h>
 #include <string.h>
 #include <alloca.h>
