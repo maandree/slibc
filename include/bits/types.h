6f893eb15444fe7697a1808e9cdc9cf6bdd372bb
Mattias Andrée <maandree@operamail.com>
2015-11-20 02:13:21 +0100

    typo
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- include/bits/types.h
+++ include/bits/types.h
@@ -25,7 +25,7 @@
 #include <bits/intconf.h>
 /* TODO [u]int(|_least|_fast)$N_t are compiler-dependent and may even
  *      be compiler-flag-dependent. Some CPP-magic is required to
- *      figure there exact underlaying intrinsic type and existence. */
+ *      figure their exact underlaying intrinsic type and existence. */
 
 
 /* Ensure that dependencies for type are defined. */
