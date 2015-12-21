e46cbf0518918c46e541ee114b1b89c6f17b107c
Mattias Andrée <maandree@member.fsf.org>
2015-12-21 14:12:53 +0100

    add creat
    Signed-off-by: Mattias Andrée <maandree@member.fsf.org>

---
+++ src/fcntl/creat.c
@@ -0,0 +1,44 @@
+/**
+ * slibc — Yet another C library
+ * Copyright © 2015  Mattias Andrée (maandree@member.fsf.org)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <fcntl.h>
+
+
+
+/**
+ * Create a new file and open it for writing, or
+ * if the file already exists, open it for rewriting,
+ * in which the file will be emptied.
+ * 
+ * `creat(path, mode)` is identical to
+ * `open(path, O_WRONLY | O_CREAT | O_TRUNC, mode)`.
+ * 
+ * @etymology  (Creat)e file!
+ * 
+ * @param   path  The pathname of the file to open.
+ * @param   mode  File permissions for the new file.
+ * @return        The file descriptor to the opened file, -1 on error.
+ * 
+ * @throws  Any error specified for open(3) except for `EEXIST`.
+ * 
+ * @since  Always.
+ */
+int creat(const char* path, mode_t mode)
+{
+  return open(path, O_WRONLY | O_CREAT | O_TRUNC, mode);
+}
+
