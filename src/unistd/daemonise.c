35fa9b70d36d85a72582b2713e7205d5156280ee
Mattias Andrée <maandree@member.fsf.org>
2015-12-23 15:42:17 +0100

    daemonise: may also be redistributed under the term of the expat license
    Signed-off-by: Mattias Andrée <maandree@member.fsf.org>

--- src/unistd/daemonise.c
+++ src/unistd/daemonise.c
@@ -5,7 +5,9 @@
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * (at your option) any later version. As a special exception for this
+ * file, you may also redistribute it and/or modify it under the terms
+ * of the Expat License.
  * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
